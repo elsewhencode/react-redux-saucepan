c3eefbe88c8dde0bd355300feb813fab71159da1
Vahid Panjganj <vahid@wearehive.co.uk>
2018-04-24 17:01:12 +0100

    Remove old flow-typed defs

--- flow-typed/redux-form.js
+++
@@ -1,7 +0,0 @@
-// done to prevent redux-form. Required module not found
-// read more here: https://github.com/facebook/flow/issues/2092
-// remove whenredux-form resolved this issue
-
-declare module 'redux-form' {
-  declare module.exports: any;
-}
