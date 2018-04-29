c1ede581af4ab1ba1e39362ef4e9add2c6119939
Vahid Panjganj <vahid@wearehive.co.uk>
2018-04-29 16:15:56 +0100

    Add react 16 error boundary

---
+++ src/client/ErrorBoundry.js
@@ -0,0 +1,41 @@
+// @flow
+import React, { type Node, Fragment } from 'react';
+
+type Props = {
+  children?: Node,
+};
+
+type State = {
+  hasError: boolean,
+  error: ?{},
+  info: ?{ componentStack: string },
+};
+
+export default class ErrorBoundary extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { hasError: false, error: null, info: null };
+  }
+
+  componentDidCatch(error: {}, info: { componentStack: string }) {
+    // Display fallback UI
+    this.setState({ hasError: true, error, info });
+    // You can also log the error to an error reporting service
+    // logErrorToMyService(error, info);
+  }
+
+  render() {
+    const { hasError, error, info } = this.state;
+    if (hasError) {
+      // You can render any custom fallback UI
+      return (
+        <Fragment>
+          <h3>React error</h3>
+          <pre>{error && error.toString()}</pre>
+          <pre>{info && ('No componentStack info' || info.componentStack)}</pre>
+        </Fragment>
+      );
+    }
+    return this.props.children;
+  }
+}
