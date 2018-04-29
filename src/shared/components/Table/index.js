b0a6f607f795ec2c80a66aec39028a8742b545e3
Vahid Panjganj <vahid@wearehive.co.uk>
2018-04-29 16:21:26 +0100

    Stop the front end from double fetching server
    - added a new state attribute `dataFetchedByServer`
    - new action (`PRE_LOADED_DATA_IS_VIEWED`) to update
    `dataFetchedByServer`
    - check the attribute on `Table` component

--- src/shared/components/Table/index.js
+++ src/shared/components/Table/index.js
@@ -1,21 +1,70 @@
 // @flow
-import React, { type Element } from 'react';
+import React, { Component, Fragment } from 'react';
 
+import Loading from './../Loading';
+import type { RepoType } from '../../reducers/reposList';
+
+// // FIXME: there is a flow-typed issue that gives flow error per each defined prop type here
 type Props = {
-  reposList: {}[],
+  data: RepoType[],
+  loading: boolean,
+  dataFetchedByServer: boolean,
+  preLoadedDataIsViewd: () => mixed,
+  fetchReposList: () => mixed,
 };
 
-const Table = ({ reposList }: Props): Element<any> => (
-  <table>
-    <thead>
-      <tr>{Object.keys(reposList[0] || {}).map(repoItemKey => <td>{repoItemKey}</td>)}</tr>
-    </thead>
-    <tbody>
-      {reposList.map(repoItem => (
-        <tr>{Object.keys(repoItem).map(repoItemKey => <td>{repoItem[repoItemKey]}</td>)}</tr>
-      ))}
-    </tbody>
-  </table>
-);
+export default class Table extends Component<Props> {
+  componentDidMount() {
+    const { dataFetchedByServer, fetchReposList } = this.props;
 
-export default Table;
+    // first time visiting this component, no need to fetch the data
+    if (!dataFetchedByServer) {
+      fetchReposList();
+    }
+  }
+
+  componentWillUnmount() {
+    const { preLoadedDataIsViewd, dataFetchedByServer } = this.props;
+
+    // serverside renedered data is old now, next time it will be fetched by client
+    if (dataFetchedByServer) {
+      preLoadedDataIsViewd();
+    }
+  }
+
+  render() {
+    const { data, loading, dataFetchedByServer } = this.props;
+
+    return (
+      <Fragment>
+        <p>
+          Component and its data were served by{' '}
+          {dataFetchedByServer ? 'server ' : 'client '}. Data fetch did{' '}
+          {dataFetchedByServer ? ' not' : ''} happen on componentDidMount
+        </p>
+
+        {loading && <Loading />}
+        <table border="1">
+          <thead>
+            <tr>
+              <td>name</td>
+              <td>id</td>
+              <td>stars</td>
+              <td>forks</td>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map(repoItem => (
+              <tr key={repoItem.id}>
+                <td>{repoItem.name}</td>
+                <td>{repoItem.id}</td>
+                <td>{repoItem.stargazers_count}</td>
+                <td>{repoItem.forks_count}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </Fragment>
+    );
+  }
+}
--- src/shared/reducers/reposList.js
+++ src/shared/reducers/reposList.js
@@ -2,21 +2,28 @@
 import get from './../../utils/apiCall';
 
 // action constants
-export const GET_REPOS_LIST_REQUEST: 'GET_REPOS_LIST_REQUEST' = 'GET_REPOS_LIST_REQUEST';
-export const GET_REPOS_LIST_SUCCESS: 'GET_REPOS_LIST_SUCCESS' = 'GET_REPOS_LIST_SUCCESS';
-export const GET_REPOS_LIST_FAILURE: 'GET_REPOS_LIST_FAILURE' = 'GET_REPOS_LIST_FAILURE';
+export const GET_REPOS_LIST_REQUEST: 'GET_REPOS_LIST_REQUEST' =
+  'GET_REPOS_LIST_REQUEST';
+export const GET_REPOS_LIST_SUCCESS: 'GET_REPOS_LIST_SUCCESS' =
+  'GET_REPOS_LIST_SUCCESS';
+export const GET_REPOS_LIST_FAILURE: 'GET_REPOS_LIST_FAILURE' =
+  'GET_REPOS_LIST_FAILURE';
+export const PRE_LOADED_DATA_IS_VIEWED: 'PRE_LOADED_DATA_IS_VIEWED' =
+  'PRE_LOADED_DATA_IS_VIEWED';
 
 // Typing Redux state immutability by adding +
 
 export type RepoType = {
   +name: string,
   +id: number,
+  +stargazers_count: number,
+  +forks_count: number,
 };
 
 // didn't use 'exact' for this reason https://github.com/facebook/flow/issues/2405
 export type ReposListStateType = {
   +loading: boolean,
-  +loaded: boolean,
+  +dataFetchedByServer: boolean,
   +error: ?string,
   +data: RepoType[],
 };
@@ -24,70 +31,89 @@ export type ReposListStateType = {
 // Using disjoint unions, Flow will be able to understand your reducers much better.
 export type ReposListActionTypes =
   | {| +type: typeof GET_REPOS_LIST_REQUEST |}
+  | {| +type: typeof PRE_LOADED_DATA_IS_VIEWED |}
   | {| +type: typeof GET_REPOS_LIST_SUCCESS, payload: RepoType[] |}
   | {| +type: typeof GET_REPOS_LIST_FAILURE, error: string |};
 
 // FIXME: check if state is partial
 type GetState = () => ReposListStateType;
 type PromiseAction = Promise<ReposListActionTypes>;
-type Dispatch = (action: ReposListActionTypes | PromiseAction | Array<ReposListActionTypes>) => any;
+type Dispatch = (
+  action: ReposListActionTypes | PromiseAction | Array<ReposListActionTypes>,
+) => any;
 type ThunkAction = (dispatch: Dispatch, getState: GetState) => any;
 
 export const initialState: ReposListStateType = {
   loading: false,
-  loaded: false,
+  dataFetchedByServer: __SERVER__,
   error: null,
   data: [],
 };
 
-// Reducer
-export default function reducer(
-  state: ReposListStateType = initialState,
+// Reducer type
+export type ReposListReducerType = (
+  ReposListStateType,
   action: ReposListActionTypes,
-): ReposListStateType {
+) => ReposListStateType;
+// Reducer
+const reducer: ReposListReducerType = (state = initialState, action) => {
   switch (action.type) {
     case GET_REPOS_LIST_REQUEST:
       return {
         ...state,
         loading: true,
-        loaded: false,
         error: null,
       };
     case GET_REPOS_LIST_SUCCESS:
       return {
         ...state,
         loading: false,
-        loaded: true,
+        // let client know data is preloaded by server and only for one time
+        // there is no need to fetch again on componentDidMount
+        // (preventing double fetch when you land on the page)
+        dataFetchedByServer: __SERVER__, // Am i on server side?
         data: action.payload,
+      };
+    case PRE_LOADED_DATA_IS_VIEWED:
+      return {
+        ...state,
+        dataFetchedByServer: false,
       };
     case GET_REPOS_LIST_FAILURE:
       return {
         ...state,
         loading: false,
-        loaded: false,
         error: action.error,
       };
     default:
       return state;
   }
-}
+};
+
+export default reducer;
 
 // The thunk can be used to delay the dispatch of an action,
 // The inner function receives the store methods dispatch and getState as parameters.
-export function fetchReposList(): ThunkAction {
+export function fetchReposList(org: string = 'facebook'): ThunkAction {
   return async (dispatch) => {
     dispatch({ type: GET_REPOS_LIST_REQUEST });
 
     try {
-      const { body } = await get('/api/chart');
+      // TODO: set endpoint baseline based on config and env
+      const { body }: { body: RepoType[] } = await get(`https://api.github.com/orgs/${org}/repos`);
+
       dispatch({ type: GET_REPOS_LIST_SUCCESS, payload: body });
     } catch (error) {
-      console.log(JSON.stringify(error, null, 2));
       // sending generic error message
       dispatch({
         type: GET_REPOS_LIST_FAILURE,
         error: 'We cannot load the list of repos',
       });
     }
+  };
+}
+export function preLoadedDataIsViewd(): ReposListActionTypes {
+  return {
+    type: PRE_LOADED_DATA_IS_VIEWED,
   };
 }
