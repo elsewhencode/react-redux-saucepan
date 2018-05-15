e302257867f93e214315d872692376558fee7f6f
Vahid Panjganj <vahid@wearehive.co.uk>
2018-05-15 17:28:28 +0100

    Tweak flow config
    - add flow coverage config
    - improve flow config

--- src/config.js
+++ src/config.js
@@ -1,10 +1,18 @@
+// @flow
 // todo: get most of the values from env variables
 const { parsed } = require('dotenv').config();
 
 const { PORT, HOST } = parsed || {};
 
-const config = {
-  WEB_PORT: PORT || '3000',
+type ConfigType = {
+  WEB_PORT: number,
+  ASSETS_PATH: string,
+  HOST: string,
+};
+
+const config: ConfigType = {
+  // PORT will be of type String when loaded into our program
+  WEB_PORT: parseInt(PORT, 10) || 3000,
   ASSETS_PATH: '/assets/',
   HOST: HOST || '0.0.0.0',
 };
